cc1be7bcb7aac3daa82b6bca712468b47861ea90
skss-1 <110411952+skss-1@users.noreply.github.com>
2022-08-25 08:21:39 +0300

    Added digram for project view and store modules. Updgraded ESLint rules, runned lint autofix for the project. (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
