0d94a3efaa3b985ee1da2d271beecc5e48a80aa1
Oleksandr Bielik <oleksandrbielik.postmail@gmail.com>
2022-08-27 18:07:54 +0300

    removed extra wrapper from AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,23 @@
 <template>
   <header>
-    <div class="header-wrapper">
+    <nav>
       <div class="container">
-        <nav>
-          <div class="nav-wrapper">
-            <a
-              class="logo-link"
-              href="/"
+        <div class="nav-wrapper">
+          <a
+            class="logo-link"
+            href="/"
+          >
+            <img
+              class="logo-image"
+              src="../assets/logo.svg"
+              alt="logo"
+              width="50"
             >
-              <img
-                class="logo-image"
-                src="../assets/logo.svg"
-                alt="logo"
-                width="50"
-              >
-              <span class="logo-title">CINEMA-PROJECT</span>
-            </a>
-          </div>
-        </nav>
+            <span class="logo-title">CINEMA-PROJECT</span>
+          </a>
+        </div>
       </div>
-    </div>
+    </nav>
     <search-form />
   </header>
 </template>
@@ -55,10 +53,6 @@ export default {
 }
 
 nav {
-  padding: 0;
-}
-
-.header-wrapper {
   padding: 20px 0;
   background: linear-gradient(90deg, rgba(57,117,11,1) 0%, 
           rgba(121,193,66,1) 35%, rgb(177, 245, 125) 100%);
