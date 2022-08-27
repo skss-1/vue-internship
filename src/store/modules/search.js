022a2acaa419f7d20d05ae53ffdbd22bf1bf992a
nikitius20 <zaika.nikita.01@gmail.com>
2022-08-27 11:59:47 +0300

    add fetchPopularMovies and getItems in store

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -1,20 +1,31 @@
-import axios from 'axios'
-import { path } from '../../api/tmdb-api'
+import axios from 'axios';
+import { path } from '../../api/tmdb-api';
 
 export const search = {
   namespaced: true,
   state: () => ({
-    itemsList: []
+    itemsList: [],
   }),
   mutations: {
     setItems(state, payload) {
-      state.itemsList = [...payload]
-    }
+      state.itemsList = [...payload];
+    },
   },
   actions: {
     async searchMovie({ commit }, { searchValue, adultIncluded }) {
-      const res = await axios.get(`${path}/search/movie?api_key=${process.env.VUE_APP_API_KEY}&include_adult=${adultIncluded}&query=${searchValue}`)
-      commit('setItems', res.data.results)
-    }
+      const res = await axios.get(
+        `${path}/search/movie?api_key=${process.env.VUE_APP_API_KEY}&include_adult=${adultIncluded}&query=${searchValue}`
+      );
+      commit('setItems', res.data.results);
+    },
+    async fetchPopularMovies({ commit }) {
+      const res = await axios.get(
+        `${path}/movie/popular?api_key=${process.env.VUE_APP_API_KEY}`
+      );
+      commit('setItems', res.data.results);
+    },
+  },
+  getters:{
+    getItems:(state) => state.itemsList
   }
-}
+};
