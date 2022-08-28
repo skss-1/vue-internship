b46bedf0388af51f193c864883915e00ba8148a5
nikitius20 <zaika.nikita.01@gmail.com>
2022-08-28 17:23:50 +0300

    Created a route to MoviePage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import MoviePage from '../components/MoviePage';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/movie/:id',
+    name: 'movie-page',
+    component: MoviePage,
   },
   {
     path: '/about',
