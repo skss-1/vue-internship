d2e2f70852132363dc88153b64fdb1cf217bb3e4
nikitius20 <zaika.nikita.01@gmail.com>
2022-09-03 19:24:50 +0300

    Css Changes for Person Page

--- src/views/PersonPageView.vue
+++ src/views/PersonPageView.vue
@@ -1,10 +1,75 @@
 <template>
-  <div class="person-page">
-    {{ person.name }}
+  <div class="person-page py-5">
+    <div class="person-info container">
+      <div class="row">
+        <div class="col-4">
+          <img
+            :src="imageUrl"
+            class="card-img-top"
+            :alt="`${person.name}-image`"
+          >
+        </div>
+        <div class="col-8">
+          <p class="name h1">
+            {{ person.name }}
+          </p>
+          <p class="place-of-birth h2 fw-normal">
+            {{ person.place_of_birth }}
+          </p>
+          <p class="birthday h2 fw-normal">
+            Birthday {{ birthdayDate }}
+          </p>
+          <p
+            v-if="person.deathday"
+            class="deathday h2 fw-normal"
+          >
+            Deathday {{ deathdayDate }}
+          </p>
+          <div class="fs-3 biography-heading px-2 my-2"> 
+            Biography  
+          </div> 
+          <p class="fs-6 fw-light biography m-0"> 
+            {{ person.biography }} 
+          </p> 
+        </div>
+      </div>
+    </div>
+    <div class="person-credits p-5">
+      <p class="h2 roles-heading fw-normal text-center">
+        Roles
+      </p>
+      <div class="d-flex flex-wrap justify-content-center gap-3">
+        <div
+          v-for="role in credits"
+          :key="role.id"
+          class="role-card"
+          @click="goToMoviePage(role.id)"
+        >
+          <img
+            :src="roleImageUrl(role.poster_path)"
+            class="card-img-top flex-grow-1"
+            :alt="`${role.character}-image`"
+          >
+          <div class="card-body p-1 flex-grow-0">
+            <p class="title text-center my-1">
+              {{ role.original_title }}
+            </p>
+            <p class="fs-6 character-heading text-center fw-lighter my-0 mx-2">
+              Role :
+            </p>
+            <p class="character text-center fw-lighter">
+              {{ role.character }}
+            </p>
+          </div>
+        </div> 
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { posterPath } from '@/api/tmdb-api'
+
 export default {
     name: 'PersonPageView',
     computed: {
@@ -13,14 +78,69 @@ export default {
         },  
         credits() {
             return this.$store.getters['person/getCredits']
-        }  
+        },
+        imageUrl() {
+            return `${posterPath}${this.person.profile_path}`
+        },
+        birthdayDate() {
+            const month= ['January','February','March','April','May','June','July','August','September','October','November','December'];
+            let date=`${month[+this.person.birthday.slice(5,7) -1]} ${+this.person.birthday.slice(8,10)} ${this.person.birthday.slice(0,4)}`;
+            return date
+        },
+        deathdayDate() {
+            const month= ['January','February','March','April','May','June','July','August','September','October','November','December'];
+            let date=`${month[+this.person.deathday.slice(5,7) -1]} ${+this.person.deathday.slice(8,10)} ${this.person.deathday.slice(0,4)}`;
+            return date
+        }
     },
     mounted() {
-        this.$store.dispatch('person/fetchPerson',{ id:this.$route.params.id });
+        this.$store.dispatch('person/fetchPerson',{ id: this.$route.params.id });
+    },
+    methods: {
+        roleImageUrl(url) {
+            return `${posterPath}${url}`
+        },
+        goToMoviePage(id) {
+            this.$router.push({ name: 'movie-page', params: { id: id } });
+        }
     },
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.person-page{
+    background-color: #13152E;
+    color: #fff;
+}
+.role-card{
+    width: 150px;
+    border: 1px solid rgb(152, 130, 130);
+    border-radius: 10px;
+    overflow: hidden;
+    transition-duration: 0.2s;
+    .card-img-top{
+        border-radius: 10px;
+        width: 100%;
+        height: 60%;
+        object-fit: cover;
+        display: block;
+    }
+    .card-body{
+        height: 40%;
+    } 
+    &:hover{
+    transform: scale(1.05);
+    box-shadow: 0 0px 1rem 0px rgba(255, 255, 255, 0.4);
+  }
+}
+.biography-heading{
+    border-left: 1px solid #fff;
+}
+.character-heading{
+    border-top: 3px double #fff;
+}
+.roles-heading{
+    margin-bottom: 20px;
+    border-bottom: 3px solid #202246;
+}
 </style>
