9b0d07616f723328ca58e21224cfb6664129779b
nikitius20 <zaika.nikita.01@gmail.com>
2022-09-13 01:04:56 +0300

    Add a Video component for movieView

---
+++ src/components/VideosScroll.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="videos-scroll my-5">
+    <div class="fs-3 video-heading m-3 text-center"> 
+      Rlated Videos 
+    </div>
+    <div class="video container text-center p-0">
+      <iframe
+        class="trailer"
+        :src="`https://www.youtube.com/embed/${videos[currentVideo].key}`"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </div>
+    <div class="controls d-flex justify-content-center gap-3 my-2">
+      <button
+        type="button"
+        class="btn-prev btn btn-light btn-md px-3"
+        @click="prev"
+        @keydown.enter.space="prev"
+      >
+        Prev
+      </button>
+      <button
+        type="button"
+        class="btn-next btn btn-light btn-md px-3"
+        @click="next"
+        @keydown.enter.space="next"
+      >
+        Next
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideosScroll',
+  props: {
+    videos: {
+      type: Array,
+      required: true,
+    }
+  },
+  data() {
+    return {
+        currentVideo: 0,
+    }
+  },
+  methods: {
+    next() {
+      console.log('next' + this.currentVideo)
+      console.log('next' + this.currentVideo)
+      this.currentVideo < this.videos.length && this.currentVideo++;
+    },
+    prev() {
+      console.log('prev' + this.currentVideo)
+      this.currentVideo > 0 && this.currentVideo--;
+    },
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+.trailer{
+  width: 70vw;
+  height: calc(70vw / 1.73);
+  max-width: 100%;
+}
+.videos-scroll{
+  border-right: 15px double #fff;
+  border-left: 15px double #fff;
+  button{
+    color: #020916;
+    border-color: #020916;
+  }
+}
+</style>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -45,7 +45,11 @@
         </div> 
         <p class="fs-5 fw-lighter overview m-0"> 
           {{ item.overview }} 
-        </p> 
+        </p>
+        <videos-scroll
+          v-if="videos"
+          :videos="videos"
+        />
         <div class="fs-4 credits-heading m-3 "> 
           Credits 
         </div>
@@ -68,12 +72,14 @@
 </template>
 <script>
 import CreditsScroll from '@/components/CreditsScroll.vue';
+import VideosScroll from '@/components/VideosScroll.vue';
 import { posterPath } from '@/api/tmdb-api';
 
 export default {
   name: 'MovieView',
   components: {
     CreditsScroll,
+    VideosScroll,
   },
   computed: {
     item() {
@@ -91,7 +97,10 @@ export default {
     releaseDate() {
       const releaseDate = new Date(Date.parse(this.item.release_date));
       return releaseDate.toLocaleString('en', { dateStyle: 'long' });
-    }
+    },
+    videos() {
+      return this.$store.getters['movie/getVideos'];
+    },
   },
    mounted() {
     this.$store.dispatch('movie/fetchMovieDetails',{ id:this.$route.params.id });
